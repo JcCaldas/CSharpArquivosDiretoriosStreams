a29f2ab1ba1abaf76f53e0327c337ecab376e60f
JcCaldas <jessencaldas@gmail.com>
2023-08-02 06:58:27 -0300

    Alteração exercício 03 e add exercício 04

--- Exercicio03/Program.cs
+++ Exercicio03/Program.cs
@@ -10,26 +10,29 @@ try
 
     //criar arquivo
     Console.WriteLine("Criando arquivo\n");
-    using FileStream fs = new FileStream(caminhoOrigem, FileMode.Create, FileAccess.Write);
-    
-    //escrever no arquivo primeria parte
-    Console.WriteLine("Escrevendo parte 1\n");
-    using StreamWriter sw = new StreamWriter(caminhoOrigem, true);
-    sw.WriteLine("Batatinha quando nasce, espalha rama pelo chão...");
+    using (StreamWriter sw = File.CreateText(caminhoOrigem))
+    {
+
+        //escrever no arquivo primeria parte
+        Console.WriteLine("Escrevendo parte 1\n");
+        sw.WriteLine("Batatinha quando nasce, espalha rama pelo chão...");
+
+    }
 
     //escrever no arquivo segunda parte
     Console.WriteLine("Escrevendo parte 2\n");
-    using StreamWriter sw2 = new StreamWriter(caminhoOrigem, true);
-    sw2.WriteLine("Quando estudo C#, eu fico felizão!");
+    using (StreamWriter sw = File.AppendText(caminhoOrigem))
+    {
+        sw.WriteLine("Quando estudo C#, eu fico felizão!");
+
+    }
 
     //exibição de conteúdo no tela
-    Console.WriteLine("Escrevendo parte 2\n");
-    using StreamReader sr = new StreamReader(caminhoOrigem);
-
-    string? linha;
-    while((linha = sr.ReadLine()) != null)
+    Console.WriteLine("Lendo...\n");
+    using (StreamReader sr = File.OpenText(caminhoOrigem))
     {
-        Console.WriteLine(linha);
+        string conteudo = sr.ReadToEnd();
+        Console.WriteLine(conteudo);
     }
 
 }
---
+++ Exercicio04/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Exercício 04\n");
+/*Escreva um programa que leia o conteúdo de um arquivo de texto e o criptografe usando o algoritmo de
+criptografia AES. Em seguida, salve o arquivo criptografado em um novo arquivo. O nome do arquivo de
+origem e destino deve ser fornecido pelo usuário*/
+
+try
+{
+
+    string caminhoOrigem = @"..\txt";
+
+    Console.WriteLine("Digite o nome do arquivo de origem:");
+    string arquivoOrigem = Console.ReadLine() + ".txt";
+    string caminhoCompletoOrigem = Path.Combine(caminhoOrigem, arquivoOrigem);
+
+    using StreamReader sr = new StreamReader(caminhoCompletoOrigem);
+    Console.WriteLine("\nConteúdo do arquivo:");
+    Console.WriteLine($"'{sr.ReadToEnd()}'\n");
+
+    Console.WriteLine("Digite o nome do arquivo de cópia:");
+    string arquivoDestino = Console.ReadLine() + ".txt";
+    string caminhoCompletoDestino = Path.Combine(caminhoOrigem, arquivoDestino);
+
+    //Falta criptografar
+    //lembrete, criptografar o caminhoCompletoDestino antes de copiar.
+
+    File.Copy(caminhoCompletoOrigem, caminhoCompletoDestino);
+
+
+
+
+
+
+}
+
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
+finally
+{
+    Console.WriteLine("\nFim do processamento...");
+}
+
+
+
+Console.ReadKey();
